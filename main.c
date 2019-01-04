7bdbb8247b56ac8027131189651325a4a9b8e85d
Matthew T Geiger <matthewgeiger99@gmail.com>
2019-01-04 06:33:37 -0800

    Fixed opt args

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 #include "errlib.h"
 #include "mem.h"
 
@@ -19,7 +20,7 @@ main (const int argc, char *argv[])
 	data = 0x0;
 	fstype = 0x0;
 
-	while ((opt = getopt(argc, argv, "o:t:f")) != -1) {
+	while ((opt = getopt(argc, argv, "o:t:f:")) != -1) {
 		switch (opt) {
 			case 'o':
 				data = optarg;
@@ -30,23 +31,24 @@ main (const int argc, char *argv[])
 			case 'f':
 				for (j = 0; j < strlen(optarg); j++) {
 					switch (optarg[j]) {
-						case 'b': flags |= MS_BIND;
-						case 'd': flags |= MS_DIRSYNC;
-						case 'l': flags |= MS_MANDLOCK;
-						case 'm': flags |= MS_MOVE;
-						case 'A': flags |= MS_NOATIME;
-						case 'V': flags |= MS_NODEV;
-						case 'D': flags |= MS_NODIRATIME;
-						case 'E': flags |= MS_NOEXEC;
-						case 'S': flags |= MS_NOSUID;
-						case 'r': flags |= MS_RDONLY;
-						case 'c': flags |= MS_REC;
-						case 'R': flags |= MS_REMOUNT;
-						case 's': flags |= MS_SYNCHRONOUS;
+						case 'b': flags |= MS_BIND;			break;
+						case 'd': flags |= MS_DIRSYNC;		break;
+						case 'l': flags |= MS_MANDLOCK;		break;
+						case 'm': flags |= MS_MOVE;			break;
+						case 'A': flags |= MS_NOATIME;		break;
+						case 'V': flags |= MS_NODEV;		break;
+						case 'D': flags |= MS_NODIRATIME;	break;
+						case 'E': flags |= MS_NOEXEC;		break;
+						case 'S': flags |= MS_NOSUID;		break;
+						case 'r': flags |= MS_RDONLY;		break;
+						case 'c': flags |= MS_REC;			break;
+						case 'R': flags |= MS_REMOUNT;		break;
+						case 's': flags |= MS_SYNCHRONOUS;	break;
 						default: fatal("User input error");
 					}
 				}
 				break;
+
 			default:
 				fatal("User input error");
 		}
