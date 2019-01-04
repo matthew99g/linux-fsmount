3c892cae107df81e3bede566a424c49a4122ce67
Matthew T Geiger <matthewgeiger99@gmail.com>
2019-01-04 06:52:37 -0800

    Added unmount option

--- main.c
+++ main.c
@@ -14,14 +14,17 @@ main (const int argc, char *argv[])
 {
 	unsigned long flags;
 	char *data, *fstype;
-	int j, opt;
+	int j, opt, pass = 0;
 
 	flags = 0;
 	data = 0x0;
 	fstype = 0x0;
 
-	while ((opt = getopt(argc, argv, "o:t:f:")) != -1) {
+	while ((opt = getopt(argc, argv, "u:o:t:f:")) != -1) {
 		switch (opt) {
+			case 'u':
+				pass = 1;
+				break;
 			case 'o':
 				data = optarg;
 				break;
@@ -54,12 +57,19 @@ main (const int argc, char *argv[])
 		}
 	}
 
-	if (argc != optind + 2)
+	if (argc < optind)
 		fatal("Wrong number of arguments");
 
-	if (mount(argv[optind], argv[optind + 1], fstype, flags, data) == -1)
-		errnoExit("Failed to mount device\n");
+	if(!pass) {
+		if (mount(argv[optind], argv[optind + 1], fstype, flags, data) == -1)
+			errnoExit("Failed to mount device\n");
+		printf("Mounted %s on %s successfully.\n", argv[optind], argv[optind + 1]);
+	} else {
+		if(umount(argv[optind - 1]) == -1)
+			errnoExit("Failed to unmount device\n");
+		printf("Unmounted %s successfully.\n", argv[optind - 1]);
+	}
 
-	printf("Mounted %s on %s successfully.\n", argv[optind], argv[optind + 1]);
+	//printf("Mounted %s on %s successfully.\n", argv[optind], argv[optind + 1]);
 	exit(EXIT_SUCCESS);
 }
